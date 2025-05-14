cb0603fe0753a0408dcdcba1ccd76c27abb09c3e
ruizalec <ingalex140797@gmail.com>
2025-05-14 11:49:30 -0700

    Ejercicio_1

---
+++ Proy_python/Prueba.py
@@ -0,0 +1,7 @@
+Nombre = "Alex"
+Apellido = "Ruiz Calleja"
+Edad = 27
+IDempeleado = 123456
+Correo="ruizalec@globalhitss.com"
+print("Hola, mi nombre es " + Nombre + " " + Apellido + ", tengo " 
+      + str(Edad) + " años, mi ID de empleado es " + str(IDempeleado) + " y mi correo es " + Correo)

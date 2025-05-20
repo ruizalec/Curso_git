a9c08b64add16cc12b54c8914347be9fc087b126
ruizalec <ingalex140797@gmail.com>
2025-05-20 10:46:31 -0700

    Create Examen.py
    ejmplo

---
+++ Proy_python/Examen.py
@@ -0,0 +1,7 @@
+#Escribir una función que pida un número entero entre 1 y 10 y guarde 
+# en un fichero con el nombre tabla-n.txt la tabla de multiplicar de ese número, 
+# donde n es el número introducido.
+
+def guardar_tabla_multiplicar():
+    try:
+        
